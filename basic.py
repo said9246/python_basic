c5984b1d08a097b3ed2634502c79d90d50629696
said9246 <saidahmed9246@gmail.com>
2025-10-11 21:39:39 +0500

    working

--- basic.py
+++ basic.py
@@ -1,36 +1,16 @@
-# ------------data types in python----------------
+# varible topic 
+
+text=   "98"
+t=int(text)
+
 
 a=10
-b=20.5
-c=5+6j
-d="Hello"
-e=True
-f=None
-g=[1,2,3,4,5]
-h=(1,2,3,4,5)
-i={1,2,3,4,5}
-j={"name":"John", "age":30}
-k=range(5)
-l=bytes(5)
-m=bytearray(5)
-n=memoryview(bytes(5))
-print(a,b,c,d,e,f)
-print(type(a))
-print(type(b))
-print(type(c))
-print(type(d))
-print(type(e))
-print(type(f))
-print(type(g))
-print(type(h))
-print(type(i))                      
 
-print(type(j))
-print(type(k))
-print(type(l))
-print(type(m))
-print(type(n))
+print(t+a)
 
+# Multiple Assignments
+a, b, c = 1, 2, 3
+print(a, b, c)
 
-
-# --- IGNORE ---  ~
+x = y = z = "same"
+print(x, y, z)
