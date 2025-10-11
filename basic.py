40a83ab42e81ef34c370a52505a665f32b4a4318
said9246 <saidahmed9246@gmail.com>
2025-10-11 20:12:35 +0500

    first commeit

---
+++ basic.py
@@ -0,0 +1,12 @@
+x=20
+print("interger:",x)
+
+
+float=20.5
+print("float:",float)
+
+string="python"
+print("string:",string)
+
+boolean=True
+print("boolean:",boolean)
---
+++ python_basic.py
@@ -0,0 +1,3 @@
+x=50
+
+print("integer:", x)
