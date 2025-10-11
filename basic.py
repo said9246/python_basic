4c66c29858cd9371fb55341ab297947d8c9a7267
said9246 <saidahmed9246@gmail.com>
2025-10-11 22:10:44 +0500

    frst

--- basic.py
+++ basic.py
@@ -1,8 +1,17 @@
-a = [1, 2, 3]
-b = [1, 2, 3]
-c = a
+#input
 
-print(a is b)      # same object?
-print(a is c)      # same reference?
-print(2 in a)      # membership test
-print(5 not in a)
+print("Welcome to the program!")
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+
+print("Hello", name)
+print("You are", age, "years old")
+a=int(name)
+b=int(age)
+
+print(a + b)
+
+print("Goodbye!")
+print("Python", "is", "awesome", sep="-")
+print("Hello", end=" ")
+print("World!")
--- python_basic.py
+++ python_basic.py
@@ -31,3 +31,20 @@ print(type(m))
 print(type(n))
 
 # --- IGNORE ---  ~
+#input
+
+print("Welcome to the program!")
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+
+print("Hello", name)
+print("You are", age, "years old")
+a=int(name)
+b=int(age)
+
+print(a + b)
+
+print("Goodbye!")
+print("Python", "is", "awesome", sep="-")
+print("Hello", end=" ")
+print("World!")
